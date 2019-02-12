7d1ca6d03d8b92e0d32a1623289e0bed96e46a3e
kukhariev <q_h@inbox.ru>
2019-02-13 02:43:08 +0300

    chore:   modified catch block

--- src/uploadx/src/resolve_url.ts
+++ src/uploadx/src/resolve_url.ts
@@ -1,21 +1,19 @@
 export function resolveUrl(url: string, baseURI: string) {
-  if (
-    url.indexOf('//') * url.indexOf('https://') * url.indexOf('http://') ===
-    0
-  ) {
+  if (url.indexOf('//') * url.indexOf('https://') * url.indexOf('http://') === 0) {
     return url;
   }
   try {
-    return new URL(url, baseURI).href;
-  } catch {}
-
-  if (url.indexOf('/') === 0) {
-    const matches = baseURI.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/g);
-    const origin = matches && matches[0];
-    return origin + url;
-  } else {
-    const matches = baseURI.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)?(.*\/)/g);
-    const path = matches && matches[0];
-    return path + url;
+    const res = new URL(url, baseURI).href;
+    return res;
+  } catch {
+    if (url.indexOf('/') === 0) {
+      const matches = baseURI.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/g);
+      const origin = matches && matches[0];
+      return origin + url;
+    } else {
+      const matches = baseURI.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)?(.*\/)/g);
+      const path = matches && matches[0];
+      return path + url;
+    }
   }
 }
