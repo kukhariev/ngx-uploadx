eb36755705bba6d6c49d47c7957ce329d8b4f5d7
kukhariev <q_h@inbox.ru>
2019-03-31 20:22:30 +0300

    chore:  use node-uploadx

--- server/index.js
+++ server/index.js
@@ -1,196 +1,44 @@
 // @ts-check
+const DEV = process.env.NODE_ENV === 'development';
+const http = require('http');
+const url = require('url');
+const { unlinkSync } = require('fs');
+const { tmpdir } = require('os');
+const { Uploadx, DiskStorage } = require('node-uploadx');
 
-'use strict';
+const storage = new DiskStorage({ dest: (req, file) => `${tmpdir()}/ngx/${file.filename}` });
+DEV && resetStorageBeforeTest(storage);
 
-const { logger } = require('./logger');
-
-/*
- *  uploads testing server
- *
- * https://github.com/kukhariev/node-uploadx
- *
- */
-
-const bytes = require('bytes');
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
-const { uploadsDB } = require('./uploadsDB');
-const { auth } = require('./auth');
-const { errorsInjector } = require('./errorsInjector');
-
-// ----------------------------------  CONFIG  ---------------------------------
-const ENV = process.env['NODE_ENV'];
-const UPLOAD_DIR = require('os').tmpdir();
-const PORT = 3003;
-const MAX_FILE_SIZE = bytes('2gb');
-const MAX_CHUNK_SIZE = bytes('2mb');
-const ALLOWED_MIME = ['video/*', 'image/*'];
-
-// ----------------------------  CONFIGURE EXPRESS  ----------------------------
-const app = express();
-app.enable('trust proxy');
-
-process.env['NODE_ENV'] === 'development' && app.use(logger);
-
-const corsOptions = {
-  exposedHeaders: ['Range', 'Location']
-};
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-const rawBodyParser = bodyParser.raw({ limit: MAX_CHUNK_SIZE });
-
-app.delete('/upload/', auth, del);
-app.put('/upload/', errorsInjector, auth, rawBodyParser, fileChunk);
-app.use('/upload/', errorsInjector, auth, newFile);
-
-app.use(errorHandler);
-
-const server = app.listen(PORT, () => {
-  console.log(`Server listening on port: ${server.address()['port']}`);
-  console.log('MAX_FILE_SIZE: ', MAX_FILE_SIZE);
-  console.log('MAX_CHUNK_SIZE: ', MAX_CHUNK_SIZE);
-});
-process.on('SIGTERM', () => {
-  server.close(() => {
-    process.exit(0);
-  });
-});
-// ----------------------------  MDLWRs  ----------------------------
-
-/**
- *  Save chunk to disk and/or return offset for next chunk
- */
-async function fileChunk(req, res, next) {
-  if (!req.query.upload_id) {
-    return next();
+const uploads = new Uploadx({ storage });
+uploads.on('error', console.error);
+const server = http.createServer((req, res) => {
+  if (DEV && Math.random() < 0.1) {
+    res.writeHead(401, { 'Content-Type': 'text/plan' });
+    res.end('Unauthorized');
+    return;
   }
-  const upload_id = req.query.upload_id;
-  const upload = uploadsDB.findById(upload_id);
-  if (!upload) {
-    return next(404);
-  }
-  // check file/chunk size limit
-  if (+req.get('content-length') > MAX_CHUNK_SIZE) {
-    return next(413);
-  }
-  res.set('Cache-Control', 'no-store');
-  const contentRange = req.get('content-range');
-  // -------- non chunking upload --------
-  if (!contentRange) {
-    return upload.fileStream.write(req.body, async () => {
-      upload.fileStream.end();
-      const md5 = await uploadsDB.ready(upload_id);
-      res.json({ ...upload.metadata, md5 });
-    });
-  }
-  // ---------- return offset for next chunk ----------
-  if (contentRange.includes('*')) {
-    const [, total] = contentRange.match(/\*\/(\d+)/g);
-    if (+total === upload.fileStream.bytesWritten) {
-      const md5 = await uploadsDB.ready(upload_id);
-      res.json({ ...upload.metadata, md5 });
-    } else {
-      res.set('Range', `bytes=0-${upload.fileStream.bytesWritten - 1}`);
-      return res.status(308).send('Resume Incomplete');
-    }
-  }
-  // --------- append chunk data to file ---------
-  const [fm, start, end, total] = contentRange.match(/(\d+)-(\d+)\/(\d+)/).map(s => +s);
-  if (total !== upload.size) {
-    return next(400);
-  }
-  if (end + 1 < total) {
-    upload.fileStream.write(req.body, () => {
-      res.set('Range', `bytes=0-${upload.fileStream.bytesWritten - 1}`);
-      return res.status(308).send('Resume Incomplete');
-    });
+  const pathname = url.parse(req.url).pathname.toLowerCase();
+  if (pathname === '/upload') {
+    uploads.handle(req, res);
   } else {
-    return upload.fileStream.write(req.body, async () => {
-      upload.fileStream.end();
-      const md5 = await uploadsDB.ready(upload_id);
-      res.json({ ...upload.metadata, md5 });
-    });
+    res.writeHead(404, { 'Content-Type': 'text/plan' });
+    res.end('Not Found');
   }
-}
+});
 
-/**
- *  Save chunk to disk || return offset for next chunk
- */
-function newFile(req, res, next) {
-  // optional: limit file size
-  if (+req.get('x-upload-content-length') > MAX_FILE_SIZE) {
-    return next(413);
+server.listen(3003, error => {
+  if (error) {
+    return console.error('something bad happened', error);
   }
-  // optional: check  mime type
-  if (!new RegExp(ALLOWED_MIME.join('|')).test(req.get('x-upload-content-type'))) {
-    return next(415);
-  }
-  const dstpath = path.join(UPLOAD_DIR, req.body.name || req.body.title);
-  // overwrite file
-  try {
-    fs.unlinkSync(dstpath);
-  } catch (error) {}
+  console.log('listening on port:', server.address()['port']);
+});
 
-  // --------- register upload ---------
-  const upload = {
-    metadata: req.body,
-    user: req.user,
-    dstpath,
-    size: +req.get('x-upload-content-length'),
-    fileStream: fs.createWriteStream(dstpath, { flags: 'a' })
-  };
-  const upload_id = uploadsDB.save(upload).id;
-  const query = `?upload_id=${upload_id}`;
-  const location = `${req.protocol}://${req.hostname}:${PORT}${req.baseUrl}/${query}`;
-  res.location(location);
-  res.status(200).json({ location });
-}
-
-/**
- *  delete upload
- */
-function del(req, res, next) {
-  if (!req.query.upload_id) {
-    return next(404);
+function resetStorageBeforeTest(storage) {
+  const files = storage.metaStore.all;
+  for (const id in files) {
+    try {
+      unlinkSync(files[id].path);
+    } catch {}
   }
-  const upload_id = req.query.upload_id;
-  const upload = uploadsDB.findById(upload_id);
-  if (!upload) {
-    return next(404);
-  }
-  try {
-    fs.unlinkSync(upload.dstpath);
-    uploadsDB.deleted(upload_id);
-  } catch (error) {}
-  return res.json();
-}
-
-// ------------------------------  ERROR HANDLER  ------------------------------
-
-function errorHandler(err, req, res, next) {
-  const errorStatuses = {
-    400: 'Bad Request',
-    401: 'Unauthorized',
-    403: 'Forbidden',
-    404: 'Not Found',
-    413: 'Request Entity Too Large',
-    415: 'Unsupported Media Type'
-  };
-  if (typeof err === 'number') {
-    res.status(err).json({
-      error: {
-        statusCode: err,
-        message: errorStatuses[err]
-      }
-    });
-  } else {
-    err.status && console.log(err);
-    res.status(err.status).json({
-      error: err
-    });
-  }
+  storage.metaStore.clear();
 }
