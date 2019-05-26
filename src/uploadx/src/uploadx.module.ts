03f2480b0f240c3fb5477b4dba7a88a88f3c3501
kukhariev <q_h@inbox.ru>
2019-05-26 08:09:53 +0300

    feat: uploadxDrop directive

--- src/uploadx/src/uploadx.module.ts
+++ src/uploadx/src/uploadx.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UploadxDirective } from './uploadx.directive';
+import { UploadxDropDirective } from './uploadx-drop.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [UploadxDirective],
-  exports: [UploadxDirective]
+  declarations: [UploadxDirective, UploadxDropDirective],
+  exports: [UploadxDirective, UploadxDropDirective]
 })
 export class UploadxModule {}
