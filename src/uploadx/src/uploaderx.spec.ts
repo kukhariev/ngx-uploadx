7197378ca0479ed0bd70fad14090d9e27e73794e
q_h <q_h@inbox.ru>
2019-07-17 10:10:42 +0300

    test:  valid ranges

--- src/uploadx/src/uploaderx.spec.ts
+++ src/uploadx/src/uploaderx.spec.ts
@@ -28,15 +28,16 @@ describe('getRangeEnd', () => {
   it('invalid ranges', () => {
     expect(getRangeEnd(undefined)).toEqual(-1);
     expect(getRangeEnd('')).toEqual(-1);
-    expect(getRangeEnd('invalid')).toEqual(-1);
+    expect(getRangeEnd('-invalid-')).toEqual(-1);
     expect(getRangeEnd('Range: bytes=-1')).toEqual(-1);
     expect(getRangeEnd('Range: bytes=-5')).toEqual(-1);
     expect(getRangeEnd('Range: bytes=0--5')).toEqual(-1);
+    expect(getRangeEnd('Range: bytes=0--1')).toEqual(-1);
   });
   it('valid ', () => {
     expect(getRangeEnd('Range: bytes=0-1')).toEqual(1);
     expect(getRangeEnd('Range: bytes=0-0')).toEqual(0);
-    expect(getRangeEnd('Range: bytes=0--1')).toEqual(-1);
+    expect(getRangeEnd('Range: bytes=0-100')).toEqual(100);
   });
 });
 
