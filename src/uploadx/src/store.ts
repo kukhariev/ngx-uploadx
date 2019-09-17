f612e58cacc7643f90ef9fb64ba151b61d3c7c8e
kukhariev <q_h@inbox.ru>
2019-09-17 23:00:20 +0300

    refactor:  flat uploads webstorage

--- src/uploadx/src/store.ts
+++ src/uploadx/src/store.ts
@@ -1,30 +1,15 @@
 const UPLOADX_KEY = 'UPLOADX_V3.0';
 class Store<T> {
-  store: Map<string, T> = new Map<string, T>();
-  constructor(public key: string) {
-    const raw = localStorage.getItem(this.key) || '[]';
-    this.store = new Map(JSON.parse(raw));
-  }
-  clear() {
-    this.store.clear();
-    localStorage.removeItem(this.key);
-  }
+  private hasStorage = 'localStorage' in window;
+  constructor(public key: string) {}
   set(id: string, data: T) {
-    this.store.set(id, data);
-    localStorage.setItem(this.key, JSON.stringify(Array.from(this.store)));
-    return this;
+    this.hasStorage && localStorage.setItem(this.key + id, JSON.stringify(data));
   }
-  get(id: string) {
-    return this.store.get(id);
+  get(id: string): T | null {
+    return this.hasStorage && JSON.parse(localStorage.getItem(this.key + id) || 'null');
   }
   delete(id: string) {
-    const has = this.store.delete(id);
-    if (this.store.size) {
-      has && localStorage.setItem(this.key, JSON.stringify(Array.from(this.store)));
-    } else {
-      localStorage.removeItem(this.key);
-    }
-    return has;
+    this.hasStorage && localStorage.removeItem(this.key + id);
   }
 }
-export const store = new Store<string>(UPLOADX_KEY);
+export const store = new Store<string>(UPLOADX_KEY + '_');
