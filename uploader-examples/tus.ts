1e84ef15f933428bac08a746babf90ac46fc15e6
kukhariev <q_h@inbox.ru>
2019-10-07 22:22:51 +0300

    docs: add tus `Creation With Upload`  example

---
+++ uploader-examples/tus.ts
@@ -0,0 +1,36 @@
+import { b64, resolveUrl, Tus } from '../dist/uploadx/ngx-uploadx';
+
+/**
+ * Implements tus resumable upload protocol
+ * @see
+ * https://github.com/tus/tus-resumable-upload-protocol/blob/master/protocol.md
+ *
+ * Creation With Upload extension
+ */
+export class TusExt extends Tus {
+  async getFileUrl(): Promise<string> {
+    this.offset = 0;
+    this.chunkSize = this.file.size;
+    const encodedMetaData = b64.serialize(this.metadata);
+    const { body } = this.getChunk();
+    const headers = {
+      'Upload-Length': `${this.size}`,
+      'Upload-Metadata': `${encodedMetaData}`,
+      'Content-Type': 'application/offset+octet-stream',
+      'Upload-Offset': `${this.offset}`
+    };
+
+    await this.request({
+      method: 'POST',
+      url: this.endpoint,
+      headers,
+      body
+    });
+    const location = this.getValueFromResponse('location');
+    if (!location) {
+      throw new Error('Invalid or missing Location header');
+    }
+    this.offset = this.getOffsetFromResponse() || (this.responseStatus === 201 ? 0 : undefined);
+    return resolveUrl(location, this.endpoint);
+  }
+}
