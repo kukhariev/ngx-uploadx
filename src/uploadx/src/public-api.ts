f0dbc3ae1d91cec13ae2198905dad5e114780588
kukhariev <q_h@inbox.ru>
2019-10-07 22:24:40 +0300

    fix: expose utils needed for extending

--- src/uploadx/src/public-api.ts
+++ src/uploadx/src/public-api.ts
@@ -1,8 +1,8 @@
-export { ErrorHandler } from './error-handler';
+export * from './error-handler';
 export * from './interfaces';
-export { Tus } from './tus';
+export * from './tus';
 export * from './uploader';
-export { UploaderX } from './uploaderx';
+export * from './uploaderx';
 export * from './uploadx.module';
 export * from './uploadx.service';
-export { dynamicChunk } from './utils';
+export * from './utils';
