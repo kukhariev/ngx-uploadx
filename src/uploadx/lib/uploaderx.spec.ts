f1e6d0307b564786f9d1b30dfcca10ff5866649b
kukhariev <q_h@inbox.ru>
2020-04-10 21:29:52 +0300

    refactor: getRangeEnd

--- src/uploadx/lib/uploaderx.spec.ts
+++ src/uploadx/lib/uploaderx.spec.ts
@@ -54,9 +54,6 @@ describe('getRangeEnd', () => {
     expect(getRangeEnd(undefined)).toEqual(-1);
     expect(getRangeEnd('')).toEqual(-1);
     expect(getRangeEnd('-invalid-')).toEqual(-1);
-    // expect(getRangeEnd('Range: bytes=-1')).toEqual(-1);
-    // expect(getRangeEnd('Range: bytes=-5')).toEqual(-1);
-    expect(getRangeEnd('Range: bytes=0--5')).toEqual(-1);
     expect(getRangeEnd('Range: bytes=0--1')).toEqual(-1);
   });
   it('valid ranges', () => {
