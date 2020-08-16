def42271ad9cab7a07523b42582862236d44ec62
q_h <q_h@inbox.ru>
2020-08-16 20:46:00 +0300

    chore: apply gitignore

--- integrations/ng10/src/app/multi-service/multi-service.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-multi-service',
-  templateUrl: './multi-service.component.html'
-})
-export class MultiServiceComponent {}
