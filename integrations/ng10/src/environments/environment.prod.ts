adb82eabb4206f9b256d819041506215fcd82c75
q_h <q_h@inbox.ru>
2020-08-16 20:46:00 +0300

    chore: apply gitignore

--- integrations/ng10/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  api: `${window.location.protocol}//${window.location.hostname}:3003`
-};
--- integrations/ng8/src/app/service-code-way/uploader-ext.class.ts
+++
@@ -1,8 +0,0 @@
-import { UploaderX } from 'ngx-uploadx';
-
-export class UploaderExt extends UploaderX {
-  // override auth scheme
-  setAuth(encoded: string): void {
-    this.headers.Authorization = `Basic ${encoded}`;
-  }
-}
