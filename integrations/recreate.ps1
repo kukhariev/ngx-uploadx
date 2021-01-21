c0a1d4d8f54f860e968f46f31d1f9bc2af92e3a4
q_h <q_h@inbox.ru>
2021-01-22 00:17:02 +0300

    chore: fix test app init script

--- integrations/recreate.ps1
+++ integrations/recreate.ps1
@@ -2,4 +2,4 @@ npm install -g @angular/cli
 Set-Location c:\tmp
 ng new latest --style=scss --strict=true --skipInstall=true --skipGit=true --routing=true --legacyBrowsers=true
 Set-Location $PSScriptRoot
-Copy-Item -r c:\tmp\latest  latest\
+Copy-Item -r c:\tmp\latest\*  latest\
