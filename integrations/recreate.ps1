aaf1f0d8faed4aed72079dc448cfd79832693b99
q_h <q_h@inbox.ru>
2021-05-13 21:41:45 +0300

    fix: add angular v12 support (#300)
    * test: upgrade scripts
    
    * test: upgade to latest angular
    
    * fix: add angular v12 support
    
    * test: remove ie11 from browserslist
    
    * test: update build command

--- integrations/recreate.ps1
+++ integrations/recreate.ps1
@@ -1,5 +1,6 @@
 npm install -g @angular/cli
+mkdir  c:\tmp
 Set-Location c:\tmp
-ng new latest --style=scss --strict=true --skipInstall=true --skipGit=true --routing=true --legacyBrowsers=true
+ng new latest --style=scss --strict=true --skipInstall=true --skipGit=true --routing=true
 Set-Location $PSScriptRoot
 Copy-Item -r c:\tmp\latest\*  latest\
