a8f1489dd2e5fc29ddc27e3aaab8dab8372228f0
q_h <q_h@inbox.ru>
2021-09-15 15:02:35 +0300

    ci: speed up test suite (#319)
    Checks time:
    * before : 5m 36s
    * now: 1m 31s

--- integrations/recreate.ps1
+++
@@ -1,6 +0,0 @@
-npm install -g @angular/cli
-mkdir  c:\tmp
-Set-Location c:\tmp
-ng new latest --style=scss --strict=true --skipInstall=true --skipGit=true --routing=true
-Set-Location $PSScriptRoot
-Copy-Item -r c:\tmp\latest\*  latest\
