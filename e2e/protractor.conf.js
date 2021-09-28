1beea026ff1e0f94450240b23e2f9725eb60a2b8
q_h <q_h@inbox.ru>
2021-09-28 13:01:56 +0300

    ci: use installed chromedriver

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,7 +2,13 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter, StacktraceOption } = require('jasmine-spec-reporter');
+
 const chromeArgs = process.env.CI ? ['--headless'] : [];
+chromeDriver =
+  process.env.CI && process.env.CHROMEWEBDRIVER
+    ? process.env.CHROMEWEBDRIVER + '/chromedriver'
+    : '';
+
 exports.config = {
   allScriptsTimeout: 60000,
   specs: ['./**/*.e2e-spec.ts'],
@@ -12,6 +18,7 @@ exports.config = {
       args: chromeArgs
     }
   },
+  chromeDriver,
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
