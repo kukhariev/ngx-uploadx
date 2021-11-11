f415d2f172110b0f0ad2fbab13c7b73d198c983c
q_h <q_h@inbox.ru>
2021-11-11 08:24:06 +0300

    perf(dynamic-chunk): increase chunks time limits (#342)
    Changed the chunk time range from 2 - 8 sec to 8 - 24 sec.
    This noticeably reduces server and client overhead and decreases upload time.

--- src/uploadx/lib/utils.ts
+++ src/uploadx/lib/utils.ts
@@ -75,31 +75,6 @@ export const b64 = {
   }
 };
 
-/**
- * Adaptive chunk size
- */
-export class DynamicChunk {
-  /** Maximum chunk size in bytes */
-  static maxSize = Number.MAX_SAFE_INTEGER;
-  /** Minimum chunk size in bytes */
-  static minSize = 1024 * 256;
-  /** Initial chunk size in bytes */
-  static size = 4096 * 256;
-  static minChunkTime = 2;
-  static maxChunkTime = 8;
-
-  static scale(throughput: number): number {
-    const elapsedTime = DynamicChunk.size / throughput;
-    if (elapsedTime < DynamicChunk.minChunkTime) {
-      DynamicChunk.size = Math.min(DynamicChunk.maxSize, DynamicChunk.size * 2);
-    }
-    if (elapsedTime > DynamicChunk.maxChunkTime) {
-      DynamicChunk.size = Math.max(DynamicChunk.minSize, DynamicChunk.size / 2);
-    }
-    return DynamicChunk.size;
-  }
-}
-
 export function isIOS(): boolean {
   return /iPad|iPhone|iPod/.test(navigator.platform)
     ? true
