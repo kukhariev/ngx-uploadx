1fd275a83ef22c724c5e30ed69f5666a1c6f3471
kukhariev <q_h@inbox.ru>
2021-12-11 20:19:16 +0300

    test: add coverage command

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
