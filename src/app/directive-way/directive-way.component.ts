b28af1dbc2cd520d947fd13705cac7612f656ba5
kukhariev <q_h@inbox.ru>
2022-10-21 07:52:56 +0300

    feat: add `update` method and event

--- src/app/directive-way/directive-way.component.ts
+++ src/app/directive-way/directive-way.component.ts
@@ -43,6 +43,10 @@ export class DirectiveWayComponent {
     this.control = { action: 'upload', uploadId };
   }
 
+  update(uploadId?: string): void {
+    this.control = { action: 'update', uploadId, metadata: { updated: Date.now() } };
+  }
+
   onStateChanged(state: UploadState): void {
     this.state = state;
     const target = this.uploads.find(item => item.uploadId === state.uploadId);
