4615c817942f8f3b2206c7d6ee374653d36def9f
q_h <q_h@inbox.ru>
2023-01-19 15:50:40 +0300

    chore: fix `ng update` cwd

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -8,7 +8,7 @@ const { join, resolve } = require('path');
 
 const tmpDir = join(require('os').tmpdir(), 'ngx-uploadx-build');
 const baseDir = resolve(`${__dirname}/..`);
-const integrationsPath = resolve(baseDir, `integrations`);
+const integrationsPath = resolve(baseDir, 'integrations');
 process.env.NG_DISABLE_VERSION_CHECK = 'true';
 
 const cleanup = directory => new Promise(resolve => require('rimraf')(directory, resolve));
@@ -20,10 +20,10 @@ const cleanup = directory => new Promise(resolve => require('rimraf')(directory,
  *
  */
 async function build(cliTag = 'latest') {
-  const projectName = (Number.isInteger(+cliTag[0]) ? 'ng' + cliTag : cliTag).replace(/\./, '');
+  const projectName = (Number.isInteger(+cliTag[0]) ? `ng${cliTag}` : cliTag).replace(/\./, '');
   const projectPath = join(integrationsPath, projectName);
   const ngNewCmd = `ng new ${projectName} --strict --style=scss --skip-install --skip-git --routing`;
-  const ngUpdateCmd = 'ng update @angular/core --allow-dirty --migrate-only=true --from=12';
+  const ngUpdateCmd = 'npx ng update @angular/core --allow-dirty --migrate-only=true --from=12';
   const buildCmd = 'ng build --configuration production';
 
   console.info(`- Angular CLI tag: ${cliTag}`);
@@ -58,6 +58,7 @@ async function build(cliTag = 'latest') {
   await copy('dist/uploadx', `${projectPath}/node_modules/ngx-uploadx`, { overwrite: true });
 
   console.info('- Migrate project...');
+  process.chdir(projectPath);
   execSync(ngUpdateCmd, { cwd: projectPath, stdio: [0, 1, 2] });
 
   console.info(`- Running "${buildCmd}" for "${projectPath}"`);
