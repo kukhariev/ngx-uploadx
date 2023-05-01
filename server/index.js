5bc727f339526fc4ad507be828a8e2d14153564e
q_h <q_h@inbox.ru>
2023-05-01 14:55:15 +0300

    chore: update dev server

--- server/index.js
+++ server/index.js
@@ -1,47 +1,46 @@
+// @ts-check
 const args = process.argv.slice(2);
 const url = require('url');
 const { tmpdir } = require('os');
-const { DiskStorage, Multipart, Tus, Uploadx } = require('@uploadx/core');
+const { DiskStorage, Multipart, Tus, Uploadx, cors } = require('@uploadx/core');
 const { createServer } = require('http');
 
-const logLevel = process.env.LOG_LEVEL || args.includes('--debug') ? 'debug' : 'error';
 const port = process.env.PORT || 3002;
-const directory = process.env.UPLOAD_DIR || `${tmpdir()}/ngx-uploadx/`;
-const maxUploadSize = process.env.MAX_UPLOAD_SIZE || '10GB';
-const allowMIME = process.env.ALLOW_MIME?.split(',') || ['*/*'];
 const path = '/files';
-
 const pathRegexp = new RegExp(`^${path}([/?]|$)`);
+const corsHandler = cors();
 
-const storage = new DiskStorage({
-  directory,
+const config = {
   path,
-  logLevel,
-  maxUploadSize,
-  allowMIME,
-  expiration: { maxAge: '30min', purgeInterval: '5min' }
-});
-const upx = new Uploadx({ storage });
+  directory: process.env.UPLOAD_DIR || `${tmpdir()}/ngx-uploadx/`,
+  allowMIME: process.env.ALLOW_MIME?.split(',') || ['*/*'],
+  maxUploadSize: process.env.MAX_UPLOAD_SIZE || '10GB',
+  expiration: { maxAge: process.env.MAX_AGE || '1h', purgeInterval: '10min' },
+  logLevel: /** @type { 'info' } */ (
+    process.env.LOG_LEVEL || args.includes('--debug') ? 'debug' : 'error'
+  )
+};
+
+const storage = new DiskStorage(config);
+const uploadx = new Uploadx({ storage });
+const multipart = new Multipart({ storage });
 const tus = new Tus({ storage });
-const mpt = new Multipart({ storage });
 
 createServer((req, res) => {
-  const { pathname, query = { uploadType: '' } } = url.parse(req.url, true);
-  if (pathRegexp.test(pathname)) {
+  const { pathname, query = { uploadType: '' } } = url.parse(req.url ?? '', true);
+  if (pathname && pathRegexp.test(pathname)) {
     switch (query.uploadType) {
       case 'multipart':
-        mpt.handle(req, res);
+        multipart.handle(req, res);
         break;
       case 'tus':
         tus.handle(req, res);
         break;
       default:
-        upx.handle(req, res);
+        uploadx.handle(req, res);
         break;
     }
   } else {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.statusCode = 404;
-    res.end();
+    corsHandler(req, res, () => uploadx.send(res, { body: 'Not Found', statusCode: 404 }));
   }
 }).listen(port);
