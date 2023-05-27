55fe7f88e53f8ada7051654a88429103109bfe24
kukhariev <q_h@inbox.ru>
2023-05-27 15:18:40 +0300

    test: hide debug messages

--- karma.conf.js
+++ karma.conf.js
@@ -33,6 +33,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
+    browserConsoleLogOptions: { level: 'info' },
     autoWatch: true,
     browsers: ['ChromeHeadless'],
     singleRun: true,
