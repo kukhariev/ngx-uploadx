5d995ef2c96d65a292955ba435da94e3273b89df
q_h <q_h@inbox.ru>
2024-08-14 07:39:56 +0300

    refactor: `block` variant

--- src/app/service-code-way/blob-uploader .ts
+++ src/app/service-code-way/blob-uploader .ts
@@ -5,45 +5,59 @@ import { Uploader } from 'ngx-uploadx';
  * @example
  *   options: UploadxOptions = {
  *     allowedTypes: 'image/*,video/*',
- *     chunksize: 100 * 1024 * 1024,
+ *     maxChunkSize: 512 * 1024 * 1024,
  *     endpoint: `[signedURL]`,
  *     uploaderClass: BlobUploader
  *   };
  */
 export class BlobUploader extends Uploader {
+  blockList: string[] = [];
   override async getFileUrl(): Promise<string> {
-    const headers = {
-      ...commonHeaders(),
-      'x-ms-blob-type': 'AppendBlob'
-    };
     const oUrl = new URL(this.endpoint);
     oUrl.pathname = [oUrl.pathname, this.file.name].join('/');
     const url = oUrl.toString();
-    await this.request({ method: 'PUT', url, headers });
     return url;
   }
 
   override async sendFileContent(): Promise<number | undefined> {
     const { body, start, end } = this.getChunk();
-    const url = `${this.url}&comp=appendblock`;
-    const headers = {
-      ...commonHeaders(),
-      'x-ms-blob-condition-appendpos': start,
-      'x-ms-blob-condition-maxsize': this.size
-    };
-    await this.request({ method: 'PUT', body, headers, url });
+    const blockId = btoa(this.uploadId + String(start).padStart(15, '0'));
+    const url = this.url + `&comp=block&blockid=${encodeURIComponent(blockId)}`;
+    const headers = commonHeaders();
+    await this.request({ method: 'PUT', headers, url, body });
+    this.blockList.push(blockId);
+    if (end === this.size) {
+      await this.finish();
+    }
     return this.responseStatus > 201 ? start : end;
+  }
+
+  async finish() {
+    const blocks = this.blockList.map(blockId => '<Latest>' + blockId + '</Latest>').join();
+    const body = `<?xml version="1.0" encoding="utf-8"?><BlockList>${blocks}</BlockList>`;
+    const url = this.url + `&comp=blocklist`;
+    const headers = { ...commonHeaders(), 'Content-Type': 'text/xml; charset=UTF-8' };
+    await this.request({ method: 'PUT', headers, url, body });
+    return this.size;
   }
 
   override abort(): void {} // FIXME: Azurite does not support blob upload interrupts?!
 
   override async getOffset(): Promise<number | undefined> {
-    const headers = { ...commonHeaders() };
-    await this.request({ method: 'HEAD', headers, url: this.url });
-    if (this.responseStatus === 200) {
-      return Number(this.responseHeaders['content-length']) || 0;
-    }
-    this.url = '';
+    const url = this.url + `&comp=blocklist&blocklisttype=all`;
+    const headers = commonHeaders();
+    try {
+      await this.request({ headers, url });
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(this.response, 'text/xml');
+      const blocks = xmlDoc
+        .getElementsByTagName('UncommittedBlocks')[0]
+        .getElementsByTagName('Block');
+      const sizes = Array.from(blocks).map(
+        el => +(el.getElementsByTagName('Size')[0]?.textContent ?? '0')
+      );
+      return sizes.reduce((acc, v) => acc + v, 0);
+    } catch {}
     return this.offset || 0;
   }
 }
