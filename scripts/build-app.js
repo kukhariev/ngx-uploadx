77d9809798dba827c96b6a832c9c59b7feb9f2f6
q_h <q_h@inbox.ru>
2024-11-17 08:09:38 +0300

    build(app): update build script

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -57,7 +57,7 @@ async function build(cliTag = 'latest') {
 
   console.info('- Migrate project...');
   process.chdir(projectPath);
-  const ngUpdateCmd = `npx ng update @angular/core --allow-dirty --migrate-only=true --from=14 --to=${angularVersion}`;
+  const ngUpdateCmd = `npx ng update @angular/core --allow-dirty --migrate-only=true --from=17 --to=${angularVersion}`;
   execSync(ngUpdateCmd, { cwd: projectPath, stdio: [0, 1, 2] });
 
   console.info(`- Running "${buildCmd}" for "${projectPath}"`);
