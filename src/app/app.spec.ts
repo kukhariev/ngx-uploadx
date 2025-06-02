34ae41f83b25d066413a5ae1841c7bd25d5da933
q_h <q_h@inbox.ru>
2025-06-02 11:10:26 +0300

    build(app): fix for new naming convention

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,6 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app';
 
 describe('AppComponent', () => {
   let comp: AppComponent;
