3919b584f26624e62c0721dc4a769716ce60950b
q_h <q_h@inbox.ru>
2026-02-09 13:22:48 +0300

    fix: remove zone.js and preserve NgZone.run for backward compatibility
    Keep NgZone.run to avoid regressions in ZoneJS apps.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app-module';
 import { environment } from './environments/environment';
@@ -8,5 +8,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()] })
+  .bootstrapModule(AppModule, { applicationProviders: [] })
   .catch(err => console.log(err));
