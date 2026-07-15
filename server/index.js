45d7e6f8a7e4c2a146335293b43428932e45149c
q_h <q_h@inbox.ru>
2026-07-15 11:17:27 +0300

    chore(server): migrate demo server to @uploadx/core v7

--- server/index.js
+++ server/index.js
@@ -1,35 +1,40 @@
 // @ts-check
-const args = process.argv.slice(2);
-const url = require('url');
-const { tmpdir } = require('os');
-const { DiskStorage, Multipart, Tus, Uploadx, cors } = require('@uploadx/core');
+const { cors, DiskStorage, Multipart, Tus, Uploadx, fromEnv } = require('@uploadx/core');
 const { createServer } = require('http');
+const { tmpdir } = require('os');
 
-const port = process.env.PORT || 3002;
-const path = '/files';
-const pathRegexp = new RegExp(`^${path}([/?]|$)`);
-const corsHandler = cors();
+const args = process.argv.slice(2);
+const PORT = process.env.PORT || 3002;
+const basePath = '/files';
+const pathRegexp = new RegExp(`^${basePath}([/?]|$)`);
 
 const config = {
-  path,
-  directory: process.env.UPLOAD_DIR || `${tmpdir()}/ngx-uploadx/`,
-  allowMIME: process.env.ALLOW_MIME?.split(',') || ['*/*'],
-  maxUploadSize: process.env.MAX_UPLOAD_SIZE || '10GB',
-  expiration: { maxAge: process.env.MAX_AGE || '1h', purgeInterval: '10min' },
-  logLevel: /** @type { 'info' } */ (
-    process.env.LOG_LEVEL || args.includes('--debug') ? 'debug' : 'error'
-  )
+  basePath: basePath,
+  uploadDir: `${tmpdir()}/ngx-uploadx/`,
+  maxFileSize: '2GB',
+  expiration: '1h',
+  ...fromEnv(),
+  logLevel: /** @type { 'error' } */ (args.includes('--debug') ? 'debug' : 'error')
 };
 
+const corsHandler = cors();
 const storage = new DiskStorage(config);
 const uploadx = new Uploadx({ storage });
-const multipart = new Multipart({ storage });
 const tus = new Tus({ storage });
+const multipart = new Multipart({ storage });
 
 createServer((req, res) => {
-  const { pathname, query = { uploadType: '' } } = url.parse(req.url ?? '', true);
-  if (pathname && pathRegexp.test(pathname)) {
-    switch (query.uploadType) {
+  const { pathname, searchParams } = new URL(req.url ?? '', 'http://localhost');
+  if (pathname === '/healthcheck') {
+    const healthcheck = {
+      memoryUsage: process.memoryUsage(),
+      uptime: process.uptime(),
+      message: 'status 👍',
+      timestamp: Date.now()
+    };
+    corsHandler(req, res, () => uploadx.send(res, { body: healthcheck }));
+  } else if (pathname && pathRegexp.test(pathname)) {
+    switch (searchParams.get('uploadType')) {
       case 'multipart':
         multipart.handle(req, res);
         break;
@@ -43,4 +48,4 @@ createServer((req, res) => {
   } else {
     corsHandler(req, res, () => uploadx.send(res, { body: 'Not Found', statusCode: 404 }));
   }
-}).listen(port);
+}).listen(PORT);
