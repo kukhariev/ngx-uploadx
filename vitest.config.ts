2ffcef7f46e601016ef592e8ed0acce2975d471a
kukhariev <q_h@inbox.ru>
2026-07-27 23:35:48 +0300

    chore(vitest): limit test scan to src directory

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['e2e/**/*'],
+    include: ['./src/**/*.spec.ts'],
     globals: true,
     environment: 'happy-dom',
     testTimeout: 10000,
