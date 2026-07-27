cf4eb06f27bb2119eacfc0987da2f9b07233ca16
kukhariev <q_h@inbox.ru>
2026-07-27 23:35:19 +0300

    fix(ajax): parse headers with `: ` in values

--- src/uploadx/lib/ajax.ts
+++ src/uploadx/lib/ajax.ts
@@ -72,8 +72,8 @@ export class UploadxAjax {
   getResponseHeaders(xhr: XMLHttpRequest): Record<string, string> {
     const rows = xhr.getAllResponseHeaders().split(/[\r\n]+/);
     return rows.reduce((headers: Record<string, string>, current) => {
-      const [name, value] = current.split(': ');
-      name && (headers[name.toLowerCase()] = value);
+      const [name, value] = current.split(/: (.*)/);
+      name && (headers[name.toLowerCase()] = value ?? '');
       return headers;
     }, {});
   }
